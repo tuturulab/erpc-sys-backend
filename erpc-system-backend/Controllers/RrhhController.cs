d2e90e8415368fb25fe5c1ae8895c58e19b9ed92
Administrator <lagunamichael2@outlook.com>
2019-10-08 15:01:54 -0600

    Iinterview controller

---
+++ erpc-system-backend/Controllers/RrhhController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore;
+using erpc_system_backend.Models;
+using erpc_system_backend.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace erpc_system_backend 
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RrhhController : TuturuControllerBase
+    {
+
+        private readonly ErpcDbContext _context;
+
+        public RrhhController(ErpcDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetAll()
+        {
+            int companyId = int.Parse(GetTokenReadable().GetCompanyId());
+
+            var interviews = await _context.Interviews.Where(e => e.Account.AccountId == companyId).ToListAsync();
+
+            return (interviews != null) ? new JsonResult(interviews) {StatusCode = (int)HttpStatusCode.OK}
+                : new JsonResult("No interviews") {StatusCode = (int)HttpStatusCode.NoContent};
+        }
+
+        public IActionResult SetfromInterview(int id)
+        {
+            int companyId = int.Parse(GetTokenReadable().GetCompanyId());
+
+            //Move employee from interview to employee
+            
+
+            return Ok();
+        }
+    }
+}
